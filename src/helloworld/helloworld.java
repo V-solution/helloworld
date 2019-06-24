dc6343f6e43735c8c9ba28718c24a813a34a58c7
ThinkPad <ThinkPad@LAPTOP-JQ4IUSDA>
2019-06-24 14:50:15 +0800

    这是第二次提交

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -1,8 +1,8 @@
 package helloworld;
  
-public class HelloWorld {
+public class helloworld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello 第二次提交");
     }
 }
